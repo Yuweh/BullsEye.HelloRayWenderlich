111e6833a3a8d2945f45de91cfa6c6bc0f4e44c8
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-11-03 04:29:50 +0800

    Almost Finished XD

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,127 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
 
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var scoreValue: Int = 0
+    var roundValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    
+    //MARK: VIEWS
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        startNewRound()
+        
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal") //UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted") //UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft") //UIImage(named: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")//UIImage(named: "SliderTrackRight")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Functions
+    
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(scoreValue)
+        roundLabel.text = String(roundValue)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+        
+    }
 
-
+    
+    //MARK: IBActions
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert() {
+        print("Hit Me! button was pressed :D")
+        
+        let difference: Int = abs(targetValue - currentValue)
+        var points = 100 - difference
+        roundValue = roundValue + 1
+        var bonusPoints = 0
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+            bonusPoints = bonusPoints + 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            points += 50
+            bonusPoints = bonusPoints + 50
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else if  difference < 30 {
+            title = "Try Harder"
+        } else {
+            title = "Try again :D"
+        }
+        
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)" +
+        "\nThe difference is: \(difference)" +
+        "\nYou've earned \(bonusPoints) bonus points" +
+         "\nYou scored a total of \(points)points"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome!", style: .default, handler: {
+            action in
+                self.startNewRound()
+        })
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        scoreValue += points
+        
+    }
+    
+    @IBAction func startOverWasPressed(_ sender: UIButton) {
+        scoreValue = 0
+        roundValue = 0
+        startNewRound()
+    }
+    
+    
+    
+    
 }
 
